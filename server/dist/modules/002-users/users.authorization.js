00e1e127148ef8052890bfff063eb1b18da4ea4e
Mahmoud Zain <magicianmt@gmail.com>
2025-09-18 05:47:38 +0300

    Delete server/dist directory
    Delete  dist directory

--- server/dist/modules/002-users/users.authorization.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endPoints = void 0;
-const user_model_1 = require("../../DataBase/models/user.model");
-exports.endPoints = {
-    freezAccount: [user_model_1.RoleEnum.admin],
-    deleteAccount: [user_model_1.RoleEnum.admin],
-};
--- server/dist/utils/security/OTP.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOTP = void 0;
-const generateOTP = () => {
-    return Math.floor(Math.random() * 1000000).toString().padStart(6, "0");
-};
-exports.generateOTP = generateOTP;
